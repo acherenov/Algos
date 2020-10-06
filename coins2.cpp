fca59d4354b7c190830072d24a676eba7504c838
acherenov <71647231+acherenov@users.noreply.github.com>
2020-10-06 18:34:18 +0300

    Add files via upload

--- coins2.cpp
+++ coins2.cpp
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h> 
+#include <limits.h>
 
-long int min_n(long int *ar, int n);
+long int min_n(long int *ar, int n, long int *k);
 long int max_n(long int *ar, int n);
 
 int main()
@@ -9,11 +10,15 @@ int main()
   int n, count = 0;
   long int result = -1;
   long int *table;
+  long int *table_copy;
   long int *numbers;
   long int *send;
   long int s; 
   long int i, j;
-  long int check;
+  long int *send_copy;
+  long int send_i;
+  long int max;  
+  long int check; 
   
   scanf("%d", &n);
  
@@ -30,6 +35,14 @@ int main()
   }
   
   scanf("%ld", &s);
+  table = malloc((s + 1) * sizeof(long int));
+  table_copy = malloc((s + 1) * sizeof(long int));
+  send = malloc(n * sizeof(long int));
+  send_copy = malloc(n * sizeof(long int));
+  for (i = 1; i <= s; i++) {
+    table[i] = LONG_MAX;
+  }
+  table[0] = 0;
   count = 0;
   for (i = 0; i < n; i++) {
     if (numbers[i] < s) {
@@ -60,120 +73,78 @@ int main()
     }
   }
   n = count;
-  
-  if (s % max_n(numbers, n) == 0) {
-    j =  max_n(numbers, n);
-    result = s / j;
-    printf("%ld\n", result);
-    for (i = 0; i < result; i++) {
-      printf("%ld ", j);  
-    }
-    free(numbers);
-    return 0;
-  }
-  
-  if (count == 0) {
-    result = -1;
-    printf("%ld", result);
-    free(numbers);
-    return 0;
-  }
-  
-  check = 1;
-  for (i = 0; i < n; i++) {
-    if (numbers[i] % 2 == 0) {
-      check = 0;
-    }
-  }
-  
-  if (check != 0 && (s % 2) == 0) {
-    result = -1;
-    printf("%ld", result);
-    free(numbers);
-    return 0;
-  }
-  
-  table = malloc(s * sizeof(long int));
-  send = malloc(n * sizeof(long int));
-  for (i = 0; i < s; i++) {
-    table[i] = -1;
-  }
-  
-  for (i = 0; i < n; i++) {
-    table[numbers[i] - 1] = 1;
-  }
-  
-  j = max_n(numbers, n);
-  for (i = 2 * j - 1; i < s; i += j) {
-    table[i] = (i + 1) / j;
-  }
-  
-  j = min_n(numbers, n);
-  for (i = j; i < s; i++) {
-    if (table[i] == -1) { 
-      count = 0;
+  max = max_n(numbers, n);
+  for (i = 1; i <= s; i++) {
+    count = 0;
+    if (i < max) {
       for (j = 0; j < n; j++) {
         if (i - numbers[j] >= 0) {
-          if (table[i - numbers[j]] != -1) {
-            send[count] = table[i - numbers[j]]; 
-            count++;  
-          }
+          send[count] = table[i - numbers[j]];
+          send_copy[count] = numbers[j];
+          count++; 
         }
       }
       if (count == 0) {
         continue;
       } else {
-        if (min_n(send, count) != -1) {
-          table[i] = min_n(send, count) + 1;  
+        result = min_n(send, count, &send_i);
+        if (result != -1) {
+          table[i] = result + 1;  
+          table_copy[i] = send_copy[send_i];
         }
+      }
+    } else {
+      for (j = 0; j < n; j++) {
+        send[j] = table[i - numbers[j]];
+      }
+      result = min_n(send, n, &send_i);
+      if (result != -1) {
+        table[i] = result + 1;  
+        table_copy[i] = numbers[send_i];
       }
     }
   }
   
-  if (table[s-1] == -1) { 
+  if (table[s] == -1) { 
     result = -1; 
     free(send);
     free(numbers);
     free(table);
+    free(send_copy);
+    free(table_copy);
     printf("%ld", result);
     return 0;
   }
   
-  result = table[s - 1];
+  result = table[s];
   printf("%ld\n", result);
-  j = s - 1;
+  j = s;
   while (result > 0) {
-    if (table[j] == 1) {
-      printf("%ld", j + 1);
-      break;
-    }
-    for (i = 0; i < n; i++) {
-      if (j - numbers[i] >= 0){
-        if (table[j - numbers[i]] == table[j] - 1) {
-          printf("%ld ", numbers[i]);
-          result -= 1;
-          j -= numbers[i];
-        } 
-      }
-    }
+    printf("%ld ", table_copy[j]);
+    j -= table_copy[j];
+    result -= 1;
   }
   
   free(send);
   free(numbers);
   free(table);
+  free(send_copy);
+  free(table_copy);
   return 0;
   
 }
 
-long int min_n(long int *ar, int n) {
+long int min_n(long int *ar, int n, long int *k) {
   int i;
   long int min = ar[0]; 
+  *k = 0;
   for (i = 0; i < n; i++) {
     if (ar[i] == -1) {
       continue;
     }
     if (ar[i] < min) {
       min = ar[i];
+      *k = i;
     }
   }
   return min;
@@ -192,3 +163,5 @@ long int max_n(long int *ar, int n) {
   }
   return max;
 }
+
+
